45922c1f4362bfa57f5db9b75edcb801fd66f20e
Ruslan <rvslan@pm.me>
2021-12-21 21:57:51 +0100

    Select punklist component init

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![alt text](https://github.com/rvslan/react-nft-challenge/blob/main/screenshot.png?raw=true)
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
-import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Punklist from "./components/Punklist";
+import Main from "./components/Main";
 
 function App() {
   const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
 
-  useEffect (() => {
+  useEffect(() => {
     const getMyNfts = async () => {
-      const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x1370802f4A5858ceA56aAb35E48A3EFccF2Fc687&order_direction=asc');
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x1370802f4A5858ceA56aAb35E48A3EFccF2Fc687&order_direction=asc"
+      );
 
-      setPunkListData(openseaData.data.assets)
-    }
+      setPunkListData(openseaData.data.assets);
+    };
 
     return getMyNfts();
   }, []);
@@ -21,7 +24,15 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <Punklist punkListData={punkListData}/>
+      {punkListData.length > 0 && (
+        <>
+          <Main punkListData={punkListData} selectedPunk={selectedPunk} />
+          <Punklist
+            punkListData={punkListData}
+            setSelectedPunk={setSelectedPunk}
+          />
+        </>
+      )}
     </div>
   );
 }
